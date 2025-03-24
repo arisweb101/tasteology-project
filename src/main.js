9df185332a6d525103ef6030b8ffbe743b590489
Aristotle Espiritu <aristotle.espiritu@gmail.com>
2025-03-25 04:29:50 +0800

    Add modal functionality

--- src/main.js
+++ src/main.js
@@ -2,6 +2,38 @@ import './styles/main.scss';
 import lozad from 'lozad';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Modal functionality
+    const modal = document.getElementById('myModal');
+    const modalOverlay = document.getElementById('modal-overlay');
+    const modalImage = document.getElementById('modalImage');
+    const closeBtn = document.getElementsByClassName('close')[0];
+
+    const images = document.querySelectorAll('.modal-image');
+
+    images.forEach((image) => {
+        image.addEventListener('click', (e) => {
+            e.preventDefault();
+            modal.classList.add('open');
+            modalImage.src = e.target.src;
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.classList.remove('open');
+    });
+
+    window.addEventListener('click', (e) => {
+        if (e.target === modalOverlay) {
+            modal.classList.remove('open');
+        }
+    });
+
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            modal.classList.remove('open');
+        }
+    });
+
     // Lazy load images
     const observer = lozad('.lazy', {
         loaded: (el) => {
