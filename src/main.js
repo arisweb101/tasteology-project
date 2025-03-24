391bf4e1b69595a683ae6d0151ab4fae971c4dea
Aristotle Espiritu <aristotle.espiritu@gmail.com>
2025-03-25 04:26:19 +0800

    Add the base HTML and CSS for the layout

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import './styles/main.scss';
+import lozad from 'lozad';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Lazy load images
+    const observer = lozad('.lazy', {
+        loaded: (el) => {
+            el.classList.add('loaded');
+        },
+    });
+
+    observer.observe();
+});
